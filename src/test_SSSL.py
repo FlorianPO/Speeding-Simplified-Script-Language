e2a49fbf0df0764ccc3691fce34eaf5c991cb20e
FlorianPO <florian.popek@gmail.com>
2016-02-09 14:45:11 +0100

    JSON AST

--- src/test_SSSL.py
+++ src/test_SSSL.py
@@ -5,6 +5,7 @@ from __future__ import (absolute_import, division, print_function,
 from parser_SSSL import SSSLParser
 
 import sys
+import json
 
 import grako
 from grako.codegen import codegen
@@ -13,6 +14,8 @@ def main():
 	parser = SSSLParser()
 	ast = parser.parse('int a = c', rule_name='START')
 	print(ast)
+	print()
+	print(json.dumps(ast, indent=2))
 
 if __name__ == '__main__':
     main()
