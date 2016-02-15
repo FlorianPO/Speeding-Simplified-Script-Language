97a011e4853bf5bd9a13f0723c711b2002d65059
FlorianPO <florian.popek@gmail.com>
2016-02-15 14:51:24 +0100

    A SSSL input file is used

--- src/test_SSSL.py
+++ src/test_SSSL.py
@@ -10,9 +10,14 @@ import json
 import grako
 from grako.codegen import codegen
 
+import fileinput
+
 def main():
 	parser = SSSLParser()
-	ast = parser.parse('int a = c', rule_name='START')
+	
+	f = open('programme.sssl', 'r')
+	
+	ast = parser.parse(f.read(), rule_name='START')
 	print(ast)
 	print()
 	print(json.dumps(ast, indent=2))
